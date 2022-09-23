8f28ec5b1f8bb0ccdd7cc984a993f064ad505af9
LenSys <[omitted]>
2022-09-23 12:49:57 +0200

    added quote entity, repository and migration

---
+++ src/Entity/Quote.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\QuoteRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: QuoteRepository::class)]
+class Quote
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $quote = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $author = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getQuote(): ?string
+    {
+        return $this->quote;
+    }
+
+    public function setQuote(string $quote): self
+    {
+        $this->quote = $quote;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+}
